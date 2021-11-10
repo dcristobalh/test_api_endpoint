510a6129f006cf27321fdfc2b8ca47c1f13068de
Diego Cristóbal Herreros <diecristher@gmail.com>
2021-11-10 19:36:29 +0100

    Feat: flask get api created and requirements too

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route('/helloworld')
+@app.route('/helloworld', methods = ['GET'])
+def hello_world():
+    return jsonify({"hello":"world"})
