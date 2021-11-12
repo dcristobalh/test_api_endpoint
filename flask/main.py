eb47d4c0bcf405dde5f03a9e4b354d9731f79e27
Diego Cristóbal Herreros <diecristher@gmail.com>
2021-11-12 06:57:26 +0100

    Feat: add 443 listen port

--- flask/main.py
+++ flask/main.py
@@ -4,3 +4,6 @@ app = Flask(__name__)
 @app.route('/helloworld', methods = ['GET'])
 def hello_world():
     return jsonify({"hello":"world"}), 200
+
+if __name__ == "__main__":
+    app.run(ssl_context='adhoc')
